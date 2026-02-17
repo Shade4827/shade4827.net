9a30992447699e5f238468cbc3fcf9e4a72cc8f3
Shade4827 <shade4827@gmail.com>
2026-02-17 20:23:20 +0900

    ソーシャルリンクにnoteのリンクを追加

--- src/components/SocialLink.vue
+++ src/components/SocialLink.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import GitHubIcon from './icons/GithubIcon.vue'
 import XIcon from './icons/XIcon.vue'
+import NoteEditIcon from 'vue-material-design-icons/NoteEdit.vue'
 </script>
 
 <template>
@@ -10,6 +11,9 @@ import XIcon from './icons/XIcon.vue'
     </a>
     <a href="https://github.com/Shade4827" target="_blank" rel="noopener noreferrer">
       <GitHubIcon size="24" />
+    </a>
+    <a href="https://note.com/shade4827" target="_blank" rel="noopener noreferrer">
+      <NoteEditIcon />
     </a>
   </div>
 </template>
